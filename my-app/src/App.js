9fa897fe17698d49989f8bf31b33efd846415c5b
Alen-Kalac <alengames@live.com>
2023-05-04 20:38:01 +0200

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,26 @@
 import "./App.css";
-import CardPage from "./CardPage/CardPage";
+// import CardPage from "./CardPage/CardPage";
+import React, {useState} from 'react';
 // import InputCpt from "./components/InputCpt/InputCpt";
 
 
 
 
 function App() {
+  let [text,setText] = useState("")
+  const update =(e) =>{
+   setText(e.target.value)
+  }
+  
+  
+
+
   return (
     <div className="App">
-     <div className="top">
-      <h1>Watch Together!</h1>
-      <input type="text" placeholder="Search"></input>
+     <div className="counterDiv">
+    <input onChange={update}  type="text" className="textGrab" placeholder="Unesite zeljeni text"></input>
+    <h1>Uneli ste: {text} </h1>
      </div>
-     <CardPage />
     </div>
   );
 }
