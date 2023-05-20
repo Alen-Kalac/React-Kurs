19255b85cb97aa419907af7f75c03661ba8532ee
Alen-Kalac <alengames@live.com>
2023-05-20 19:35:42 +0200

    node_modules added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,28 @@
 import "./App.css";
 // import CardPage from "./CardPage/CardPage";
-import React, {useState} from 'react';
+import React, { useState } from "react";
 // import InputCpt from "./components/InputCpt/InputCpt";
 
-
-
-
 function App() {
-  let [text,setText] = useState("")
-  const update =(e) =>{
-   setText(e.target.value)
-  }
-  
   
 
+  const [loginStatus,setLoginStatus] = useState(false)
+
+  const logIn = () =>{
+  setLoginStatus(false)
+  console.log("Alen")
+
+  }
+  const logOut = () =>{
+  setLoginStatus(true)
+  console.log("Umer")
+  }
 
   return (
     <div className="App">
-     <div className="counterDiv">
-    <input onChange={update}  type="text" className="textGrab" placeholder="Unesite zeljeni text"></input>
-    <h1>Uneli ste: {text} </h1>
-     </div>
+    <div className="heading"> {loginStatus ? (  <h1>Login</h1>) : (<h1>Dobro dosli !</h1> )}
+    </div>
+    <button onClick={ loginStatus ? logIn : logOut }>{loginStatus ? ("Login") :("Logout")}</button>
     </div>
   );
 }
